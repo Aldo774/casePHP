0edf532973acff25c3f0e33d7d057cf7d0fcfefc
Aldo774 <soares_aldo@hotmail.com>
2016-08-31 03:39:54 -0300

    Layout/CSS
    criação de layout, elaboração css para case.

---
+++ index.php
@@ -0,0 +1,87 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title>HOME</title>
+	</head>
+	<body>
+		<div class="titulo">
+			<div></div>
+			<div class="wrap">
+				<h1>Titulo</h1>
+			</div>
+		</div>
+		<div class="wrap">
+			<nav class="meno">
+				<ul>
+					<li><a href="#">page1</a></li>
+					<li><a href="#">page2</a></li>
+					<li><a href="#">page3</a></li>
+					<li><a href="#">page4</a></li>
+					<li><a href="#">page5</a></li>
+				</ul>
+			</nav>
+			<aside>
+				<article>
+					<h1>Sobre</h1>
+				</article>
+				<article>
+					<h1>Midia</h1>
+				</article>
+				<article>
+					<h1>#CARROSEL N</h1>
+				</article>
+				<article>
+					<h1>Rede</h1>
+				</article>
+			</aside>		
+			<section class="conteudo">
+				<article>
+					<div>
+						<img src="#">
+					</div>
+					<div>
+						<h1>Publicacao 1</h1>
+						<p>alguma coisa</p>
+					</div>
+				</article>
+				<article>
+					<div>
+						<img src="#">
+					</div>
+					<div>
+						<h1>Publicacao 2</h1>
+						<p>alguma coisa</p>
+					</div>
+				</article>
+				<article>
+					<div>
+						<img src="#">
+					</div>
+					<div>
+						<h1>Publicacao 3</h1>
+						<p>alguma coisa</p>
+					</div>
+				</article>
+			</section>			
+		</div>
+		<footer>
+			<div class="wrap">
+				<div>
+					<div>
+						<h1>Logo</h1>
+					</div>
+					<div>
+						<h1>Contatos</h1>
+					</div>
+					<div>
+						<h1>Links</h1>
+					</div>				
+				</div>
+				<div>
+					<p>Desenvolvido para fins de estudo por ...</p>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
